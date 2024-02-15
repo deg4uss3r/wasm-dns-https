29967f1acb9de508d0a047a2fa5d582ac439f301
Ricky Hosfelt <ricky_github@hosfe.lt>
2024-02-15 11:57:47 -0500

    Hookup basic Google DNS over HTTPs and assign a service_id

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
 use bytes::Buf;
 use dns_message_parser::question;
 use fastly::http::{header, Method, StatusCode};
-use fastly::{mime, Body, Error, Request, Response};
+use fastly::{Backend, mime, Body, Error, Request, Response};
 
 use base64::prelude::*;
 use std::str;
-use wasmtime_wasi::preview2::bindings::wasi::sockets::udp::UdpSocket;
+use std::time::Duration;
 
 const BLOCKLIST: &[&str] = &["facebook.com"];
+const GOOGLEDNS: &str = "https://dns.google/resolve?name=";
+const DNSBINARY: &str = "&ct=application/dns-message";
 
 #[fastly::main]
 fn main(req: Request) -> Result<Response, Error> {
+    //dynamic backend setup
+    let backend = Backend::builder("my_backend", "http-me.glitch.me")
+    .override_host("http-me.glitch.me")
+    .connect_timeout(Duration::from_secs(1))
+    .first_byte_timeout(Duration::from_secs(15))
+    .between_bytes_timeout(Duration::from_secs(10))
+    .enable_ssl()
+    .finish()?;
+
     // Log service version
     println!(
         "FASTLY_SERVICE_VERSION: {}",
@@ -31,7 +42,7 @@ fn main(req: Request) -> Result<Response, Error> {
                 .with_body_text_plain("This method is not allowed\n"))
         }
 
-        // Let any other requests through
+        // Let any other requests through in this case literally only GET or POST
         _ => (),
     };
 
@@ -41,7 +52,7 @@ fn main(req: Request) -> Result<Response, Error> {
             let dns_request_wire = BASE64_URL_SAFE_NO_PAD.decode(base64_url).unwrap();
             let dns_request =
                 dns_message_parser::Dns::decode(dns_request_wire.clone().into()).unwrap();
-            let url = dns_request
+            let urls = dns_request
                 .questions
                 .iter()
                 .map(|question| format!("{}", question.domain_name))
@@ -57,22 +68,38 @@ fn main(req: Request) -> Result<Response, Error> {
                     let mut body = Body::new();
                     body.write_bytes(&buf);
 
-                    Ok(Response::from_status(StatusCode::OK)
-                        .with_header("Content-Type", "application/dns-message")
-                        .with_header("Content-Length", format!("{}", buf.len()))
-                        .with_header("Cache-Control", "max-age=3709")
-                        .with_body(body))
+
                 }
                 Err(e) => Ok(Response::from_status(StatusCode::OK)
                     .with_content_type(mime::TEXT_HTML_UTF_8)
                     .with_body(include_str!("./404.html"))),
-            }
-        }*/
+            }*/
 
-        // so for now we'll use Google's DNS-over-https as in intermediary 
-        // I've picked the JSON API because it has more options availble 
-        // https://developers.google.com/speed/public-dns/docs/doh/json 
-        
+            // so for now we'll use Google's DNS-over-https as in intermediary 
+            // I've picked the JSON API because it has more options availble 
+            // https://developers.google.com/speed/public-dns/docs/doh/json
+            // https://dns.google/resolve?name=www.fastly.com.&ct=application/dns-message
+            //TODO hook up Google's API
+
+            // For now only do one question
+            let req = format!("{}{}{}", GOOGLEDNS, urls[0], DNSBINARY);
+
+           println!("{req}"); 
+
+            let mut response = Request::get(req).send(backend)?;
+            println!("resp: {response:?}");
+            let body = response.take_body_bytes();
+            let content_length = response.get_content_length();
+
+            println!("{body:?}");
+
+            Ok(Response::from_status(StatusCode::OK)
+            .with_header("Content-Type", "application/dns-message")
+            .with_header("Content-Length", format!("{}", content_length.unwrap_or(0)))
+            .with_header("Cache-Control", "max-age=3709")
+            .with_body(body))
+            
+        },
         _ => Ok(Response::from_status(StatusCode::OK)
             .with_content_type(mime::TEXT_HTML_UTF_8)
             .with_body(include_str!("./404.html"))),
